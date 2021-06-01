7196741841d985aa0f56ade1e053e3c2f8809ee4
Michael Angerman <stormasm@gmail.com>
2021-06-01 10:21:36 -0700

    simplify csc and keep cscd

--- csc/csc.py
+++ csc/csc.py
@@ -1,23 +1,10 @@
-import ast
 import sys
-import symtable
 import dis
 
-from astutil import print_node, print_table
-
 filename = sys.argv[1]
-#print("Crawling file:", filename)
 
 with open(filename, "r") as f:
     source = f.read()
-
-t = ast.parse(source)
-#print(t)
-
-print_node(t)
-
-table = symtable.symtable(source, "a", "exec")
-print_table(table)
 
 co = compile(source, filename, "exec")
 dis.dis(co)
